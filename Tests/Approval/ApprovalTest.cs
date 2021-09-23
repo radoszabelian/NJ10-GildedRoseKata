70373c22006a636d9fccc5b35f23829ef3b215a5
Belian <radoszabelian@gmail.com>
2021-09-23 13:30:28 +0200

    Add project files.

---
+++ Tests/Approval/ApprovalTest.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using System;
+using System.IO;
+using System.Text;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+
+namespace GildedRose.Tests.Approval
+{
+    [UseReporter(typeof(DiffReporter))]
+    public class ApprovalTest
+    {
+        [Fact]
+        public void ThirtyDays()
+        {
+            var fakeoutput = new StringBuilder();
+            Console.SetOut(new StringWriter(fakeoutput));
+            Console.SetIn(new StringReader("a\n"));
+
+            Program.Main();
+            var output = fakeoutput.ToString();
+
+            Approvals.Verify(output);
+        }
+    }
+}
